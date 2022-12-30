a124772d9bb25ed1a9bc900ef93cfe3e9ba1891e
Davi <dedicadotech@gmail.com>
2022-12-30 15:46:50 -0300

    Update Ex 71.py

--- Aula 15/Ex 71.py
+++ Aula 15/Ex 71.py
@@ -1,4 +1,4 @@
-Crie um programa que simule o funciomaneto de um caixa eletrônico. No inicio, pergunte ao usuário qual será o valor a ser
-sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+#Crie um programa que simule o funciomaneto de um caixa eletrônico. No inicio, pergunte ao usuário qual será o valor a ser
+#sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
 
 #OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1
