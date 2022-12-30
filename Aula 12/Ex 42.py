1ecc677abd66251a305aae3700a33818f077f8c4
Davi <dedicadotech@gmail.com>
2022-12-30 14:26:21 -0300

    Update Ex 42.py
    Adicionada descrição da atividade.

---
+++ Aula 12/Ex 42.py
@@ -0,0 +1,4 @@
+#Refaça o desafio 035 dos triãngulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+#-Equilátero> todos os lados iguais
+#-isósceles: dois lados iguais
+#-Escaleno: todos os lados diferentes
