48c347241d048bd8a8d88362c1836600d4d75963
Davi <dedicadotech@gmail.com>
2022-12-30 15:43:29 -0300

    Update Ex 70.py
    Adicionada descrição da atividade.

---
+++ Aula 15/Ex 70.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar.
+#No final, mostre:
+#qual é o total gasto na compra.
+#quantos produtos custam mais de R$1000.
+#Qual é o nome do produto mais barato.
