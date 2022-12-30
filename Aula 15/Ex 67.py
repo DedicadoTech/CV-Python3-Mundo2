2873756ea5d696d30c74a4095f2fc503aae6f293
Davi <dedicadotech@gmail.com>
2022-12-30 15:36:32 -0300

    Update Ex 67.py
    Adicionada descrição da atividade.

---
+++ Aula 15/Ex 67.py
@@ -0,0 +1,2 @@
+#Faça um programa que mostre a tabauda de vários números, um de cada vez para cada valor digitado pelo usuário.
+#O progrmaa será interrompido quando o número solicitado for negativo.
