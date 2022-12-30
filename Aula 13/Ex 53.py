eb830bbc545d28425cb66f0d4871c9ff135f13c9
Davi <dedicadotech@gmail.com>
2022-12-30 15:01:42 -0300

    Update Ex 53.py
    Adicionada descrição da atividade.

---
+++ Aula 13/Ex 53.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando os espaços
+#Ex: Apos a Sopa
+#Ex: Acada da casa
+#Ex: A torre da derrota
+#Ex: O lobo ama o bolo
+#Ex: Anotaram a data da maratona
+
+
