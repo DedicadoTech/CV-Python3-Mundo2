6f69b05baf764f7894e8c7f7cf30df35ea6f96c4
Davi <dedicadotech@gmail.com>
2022-12-30 15:13:32 -0300

    Update Ex 58.py
    Adionada descrição da atividade.

---
+++ Aula 14/Ex 58.py
@@ -0,0 +1,2 @@
+#Melhore o jogo DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10.
+#só que agora o jogador vai tentar adicinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
