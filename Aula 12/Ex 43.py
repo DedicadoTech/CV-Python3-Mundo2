b072c97b1d492fb270184c8d832d5406cf6c80c6
Davi <dedicadotech@gmail.com>
2022-12-30 14:29:08 -0300

    Update Ex 43.py
    Adicionada descrição da atividade.

---
+++ Aula 12/Ex 43.py
@@ -0,0 +1,7 @@
+#Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, 
+#de acordo com a tabela a baixo:
+#-Abaixo de 18.5: Abaixo do Peso
+#-Entre18.5 e 25: Peso ideal
+#-25 até 30: Sobrepeso
+#-30 até 40: Obesidade
+#-Acima de 30: Obesidade Mórbida
